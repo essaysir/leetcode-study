834d0a4dedf0b00391a236ff4b575781c7db6d79
jsson <sonjs6789@gmail.com>
2026-08-18 09:04:01 +0900

    [WEEK 09] linked list cycle

--- linked-list-cycle/essaysir.java
+++ linked-list-cycle/essaysir.java
@@ -13,17 +13,18 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         // list 가 정상적으로 순환된다면, true 아니면 false
         // LinkedList 의 특징상, 요소가 이미 존재했던 내용을 가르키면, cycle 될 수 밖에 없다.
-        Set<ListNode> nodes = new HashSet<>();
-        ListNode cur = head;
+        ListNode slow = head; // 한 칸씩 이동
+        ListNode fast = head; // 두 칸씩 이동
 
-        while ( cur != null /* 아직 끝이 아닌 동안 */ ) {
-            if ( nodes.contains(cur)/* 이미 방문한 노드라면 */ ) {
+        while( fast != null && fast.next != null /* fast 가 null 이 아니면 진행   */ ){
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (  slow == fast /* 슬로우와 fast 가 만나면 종료  */ ){
                 return true;
             }
-            nodes.add(cur);
-            cur = cur.next;
         }
-
         return false;
     }
 
